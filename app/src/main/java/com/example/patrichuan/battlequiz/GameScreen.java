d2e3a1c2eeadb0e355f5ef7e72b6689e22d6c092
Patrichuan <Patrichuan@gmail.com>
2015-01-30 01:59:45 +0100

    - Pruebas con Fragments en GameScreen

--- app/src/main/java/com/example/patrichuan/battlequiz/GameScreen.java
+++ app/src/main/java/com/example/patrichuan/battlequiz/GameScreen.java
@@ -1,26 +1,33 @@
 package com.example.patrichuan.battlequiz;
 
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import java.util.ArrayList;
+import java.util.List;
 
 // Problema con las puntuaciones en tablets (en esos casos la fuente deberia de ser 40sp y no 20sp)
 public class GameScreen extends ActionBarActivity {
 
-    public static GameScreen_Tablero FragmentTablero;
-    public static GameScreen_Pregunta FragmentPregunta;
-    public static FragmentManager fragmentManager;
+
+    ImageView OcupadoPor, CasillaDe;
+    GridView gridView;
+    TableroAdapter tableroadapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
         setContentView(R.layout.gamescreen_layout);
@@ -33,39 +40,57 @@ public class GameScreen extends ActionBarActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        // FRAGMENTS
-        FragmentTablero = new GameScreen_Tablero();
-        FragmentPregunta = new GameScreen_Pregunta();
-        fragmentManager = getFragmentManager();
+        gridView = (GridView)findViewById(R.id.gridview);
+        tableroadapter = new TableroAdapter(this);
 
-        FragmentTransaction transaction_1 = fragmentManager.beginTransaction();
-        transaction_1.replace(R.id.child_two, FragmentTablero);
-        transaction_1.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        transaction_1.addToBackStack(null);
-        transaction_1.commit();
+        gridView.setAdapter(tableroadapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // Prueba del listener: Tag de la casilla
+                //String message = "Clicked : " + v.findViewById(R.id.Casilla).getTag();
+                //Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
 
+                // Prueba del listener: Dibujar personaje
+                OcupadoPor = (ImageView) v.findViewById(R.id.Ocupante);
+                OcupadoPor.setImageResource(R.drawable.seven_personaje_amarillo);
+
+                // Prueba del listener: Colorear casilla
+                CasillaDe = (ImageView) v.findViewById(R.id.Casilla);
+                CasillaDe.setImageResource(R.drawable.seven_casilla_amarilla);
+
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                GameScreen_Pregunta fragmentPregunta = (GameScreen_Pregunta) fragmentManager.findFragmentByTag("PopUpPregunta");
+
+                // Compruebo si el fragment esta ya creado o no, si no lo esta lo creo
+                if (fragmentPregunta == null) {
+                    fragmentPregunta = new GameScreen_Pregunta();
+                    transaction.add(R.id.child_two, fragmentPregunta, "PopUpPregunta");
+                    transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                    transaction.addToBackStack(null);
+                }
+                transaction.commit();
+            }
+        });
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_game_screen, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
+    //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -74,4 +99,67 @@ public class GameScreen extends ActionBarActivity {
         super.onBackPressed();
         overridePendingTransition(R.anim.pull_in_left, R.anim.push_out_right);
     }
+
+
+    public class TableroAdapter extends BaseAdapter {
+        private List<Item> items = new ArrayList<Item>();
+        private LayoutInflater inflater;
+
+        public TableroAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+            StringBuilder NombreCasilla = new StringBuilder("Casilla");
+            for (int i = 1; i <= 35; i++) {
+                NombreCasilla.append(i);
+                items.add(new Item(NombreCasilla.toString(), R.drawable.seven_casilla_gris_borde, R.drawable.empty));
+                NombreCasilla = new StringBuilder("Casilla");
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return items.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return items.get(i).drawableCasilla;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View v = view;
+            ImageView Casilla;
+            ImageView Ocupante;
+            if (v == null) {
+                v = inflater.inflate(R.layout.gridview_item, viewGroup, false);
+                v.setTag(R.id.Casilla, v.findViewById(R.id.Casilla));
+                v.setTag(R.id.Ocupante, v.findViewById(R.id.Ocupante));
+            }
+            Item item = (Item) getItem(i);
+            Casilla = (ImageView) v.getTag(R.id.Casilla);
+            Casilla.setImageResource(item.drawableCasilla);
+            Casilla.setTag(item.name);
+            Ocupante = (ImageView) v.getTag(R.id.Ocupante);
+            Ocupante.setImageResource(item.drawableOcupante);
+            return v;
+        }
+
+        private class Item {
+            final String name;
+            final int drawableCasilla;
+            final int drawableOcupante;
+
+            Item(String name, int drawableCasilla, int drawableOcupante) {
+                this.name = name;
+                this.drawableCasilla = drawableCasilla;
+                this.drawableOcupante = drawableOcupante;
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/patrichuan/battlequiz/GameScreen_Pregunta.java
+++ app/src/main/java/com/example/patrichuan/battlequiz/GameScreen_Pregunta.java
@@ -1,74 +1,33 @@
 package com.example.patrichuan.battlequiz;
 
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
 import android.widget.TextView;
 
-import java.io.Serializable;
-
 public class GameScreen_Pregunta extends Fragment {
-
     TextView textviewdeprueba;
     ViewGroup root;
-    String texto;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Will ignore onDestroy Method
-        setRetainInstance(true);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        onRestoreInstanceState(savedInstanceState);
-        if(root!=null){
-            //((ViewGroup)root.getParent()).removeView(root);
-            //return root;
-        } else {
-            root = (ViewGroup) inflater.inflate(R.layout.gamescreen_pregunta_fragment, container, false);
-            //root = (ViewGroup) inflater.inflate(R.layout.gamescreen_pregunta_fragment, container, false);
-        }
-
+        root = (ViewGroup) inflater.inflate(R.layout.gamescreen_pregunta_fragment, container, false);
         return root;
-    }
-
-    @Override
-    public void onSaveInstanceState(final Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("texto", textviewdeprueba.getText().toString());
-    }
-
-    //Restore saved data in onSaveInstanceState Bundle
-    private void onRestoreInstanceState(Bundle savedInstanceState){
-        if(savedInstanceState!=null){
-            texto = savedInstanceState.getString("texto");
-        }
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        textviewdeprueba =(TextView)root.findViewById(R.id.textviewdeprueba);
-        textviewdeprueba.setText("Holaaaaaaaaaaaaaaa");
-
+        textviewdeprueba = (TextView) root.findViewById(R.id.texto);
+        textviewdeprueba.setText("Fragment Pregunta LANZADO !!");
         textviewdeprueba.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                FragmentTransaction transaction_3 = GameScreen.fragmentManager.beginTransaction();
-                transaction_3.replace(R.id.child_two, GameScreen.FragmentTablero);
-                transaction_3.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-                transaction_3.addToBackStack(null);
-                transaction_3.commit();
-
+                // Si lo clikeo me lo cargo (lo saco de la pila)
+                getFragmentManager().popBackStack();
             }
         });
     }
